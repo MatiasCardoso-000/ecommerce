1c43c79c71f0440066dcffce012fb7b24d423427
MatiasCardoso-000 <matias.cardoso3001@gmail.com>
2025-04-21 16:41:59 -0300

    feat: update title in index.html and implement random product display in App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -14,6 +14,7 @@ function App() {
   }
 
   const [products, setProducts] = useState<Product[]>([]);
+  const [randomProducts, setRandomProducts] = useState<Product[]>([]);
 
   const getProducts = async () => {
     const URL = `${BACKEND_URL}/products`;
@@ -33,37 +34,53 @@ function App() {
     getProducts();
   }, []);
 
+  useEffect(() => {
+    const createRandomProducts = () => {
+      const copiedProducts = [...products];
+      const sortedProducts = copiedProducts.sort(() => Math.random() - 0.5);
+      const slicedProducts = sortedProducts.slice(0, 5);
+      
+      setRandomProducts(slicedProducts);
+    };
+
+    if (products.length > 0) {
+      createRandomProducts();
+    }
+  }, [products]);
+
   return (
     <main className=" min-h-screen items-center justify-center">
-      <section className="w-full min-h-screen  grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))]  justify-center items-center p-4">
-        {products.map((product) => {
+      <section className="w-full min-h-screen  grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))]  justify-center items-center p-4 gap-4">
+        {randomProducts.map((product) => {
           return (
-            <div
-              className="flex flex-col justify-evenly text-center m-auto gap-4 bg-white border shadow-md border-slate-100 text-slate-500 md:w-[220px] md:h-[380px] p-4 rounded-md"
-              key={product.id}
-            >
-              {" "}
-              <h3 className="truncate text-2xl font-semibold">
-                {product.title}
-              </h3>{" "}
-              <p className="truncate">{product.description}</p>{" "}
-              <p className="font-semibold">{product.price}</p>
-              <div className="flex justify-center gap-2">
-                <p className="font-bold">Categoria: </p>
-                <span className="font-semibold">{product.category}</span>
-              </div>
-              <div className="flex justify-center">
-                <img
-                  src={product.image}
-                  alt={product.title}
-                  className="w-full md:w-[50px]"
-                />
-              </div>
-              <span className="font-semibold ">{product.rating}</span>
-              <button className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 cursor-pointer">
-                Add
-              </button>
-            </div>
+            <ul>
+              <li
+                key={product.id}
+                className="flex flex-col justify-evenly text-center m-auto gap-4 bg-white border shadow-md border-slate-100 text-slate-500 md:w-[220px] md:h-[380px] p-4 rounded-md"
+              >
+                {" "}
+                <h3 className="truncate text-2xl font-semibold">
+                  {product.title}
+                </h3>{" "}
+                <p className="truncate">{product.description}</p>{" "}
+                <p className="font-semibold">{product.price}</p>
+                <div className="flex justify-center gap-2">
+                  <p className="font-bold">Categoria: </p>
+                  <span className="font-semibold">{product.category}</span>
+                </div>
+                <div className="flex justify-center">
+                  <img
+                    src={product.image}
+                    alt={product.title}
+                    className="w-full md:w-[50px]"
+                  />
+                </div>
+                <span className="font-semibold ">{product.rating}</span>
+                <button className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 cursor-pointer">
+                  Add
+                </button>
+              </li>
+            </ul>
           );
         })}
       </section>
